322e2d7e1e93c32105d5ebcb0cca589d881ceb1e
wendyzyw <cocozyw2811@gmail.com>
2018-03-19 21:27:57 +1100

    add login, account management page
    add into template and static

--- mysite/social_analytics/views.py
+++ mysite/social_analytics/views.py
@@ -9,3 +9,15 @@ def login(request):
 	
 def dataview(request):
 	return render(request, 'DataView.html')
+
+def account(request):
+	return render(request, 'Account.html')
+	
+def manage1(request):
+	return render(request, 'Manage1_privacy.html')
+
+def manage2(request):
+	return render(request, 'Manage2_Personal.html')
+
+def manage3(request):
+	return render(request, 'Manage3_social.html')
