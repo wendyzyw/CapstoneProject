9e2668741228c6aff922930723b6d671560e30eb
wendyzyw <cocozyw2811@gmail.com>
2018-03-21 18:00:25 +1100

    add models user
    config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,13 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'social_analytics.apps.SocialAnalyticsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'social_analytics',
+	# 'social_analytics',
 ]
 
 MIDDLEWARE = [
