fac581a6ce9688025813a01aa586b5786bef04fa
RegexKenny <31911769+RegexKenny@users.noreply.github.com>
2018-04-25 18:09:08 +1000

    New version
    A brand new version

---
+++ mysite/social_analytics/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.conf import settings
+
+
+# Create your models here.
+class UserInfo(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.IntegerField(blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    zip_code = models.CharField(max_length=100, blank=True)
+    gender = models.CharField(max_length=100, blank=True)
+
+    # is_superuser = models.BooleanField(default=False)
+
+    # objects = MyUserManager
+    class Meta:
+        db_table = 'UserInfo'
+
+    def __str__(self):
+        return self.username  # username and email are primary key, unique
