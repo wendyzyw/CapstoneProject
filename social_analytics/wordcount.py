cfd324d99b0c4cfadcb608806fbbed7127b552dd
yuh6 <30816250+yuh6@users.noreply.github.com>
2018-05-08 15:19:06 +1000

    Update wordcount.py

--- social_analytics/wordcount.py
+++ social_analytics/wordcount.py
@@ -1,40 +1,46 @@
-from django.shortcuts import render
-from .forms import LoginForm,ChangepassForm,EdituserinfoForm,RegisterForm,MyPasswordResetForm
-from django.contrib import auth
-import nltk
-import re
 import json
-import nltk.stem.WordNetLemmatizer
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
-lemmatizer = nltk.stem.WordNetLemmatizer()
+with open('/Users/yuhan/Desktop/data.json') as f:
+    data = json.loads(f.read())
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
 word_tokenizer = nltk.tokenize.regexp.WordPunctTokenizer()
-file_text = open('/Users/yuhan/Desktop/profile.txt', 'r')
-def word_count(file):
-    BOW = {}
-    string_list = []
-    for line in file_text:
-        line = line.rstrip()
-        line = line.replace('#', '')
-        line = line.replace(',', '')
-        line = line.replace('.', '')
-        line = line.replace(':', '')
-        line = line.replace('--', '')
-        line = line.replace('"', '')
-        line = line.replace('?', '')
-        line = line.replace('(', '')
-        line = line.replace(')', '')
-        wo = line.split(' ')
-        for word in wo:
-            word = lemmatizer.lemmatize(word.lower())
-            if word != '':
-                BOW[word] = BOW.get(word, 0) + 1
-        for word in BOW:
-            word = word.encode('ascii')
-            # print (word)
-            # print ("{text: '"+word+"\', size: "+str(BOW[word])+"},")
-            string = "{text: '" + word + "', size: " + str(BOW[word]) + "}"
-            # print string
-            string_list.append(string)
 
-    return string_list
-print (word_count(file_text))
+file_text = open('/Users/yuhan/Desktop/profile.txt', 'r')
+with open('/Users/yuhan/Desktop/data.json') as f:
+    data = json.loads(f.read())
+    
+BOW = {}
+for line in file_text:
+    line = line.rstrip()
+    line = line.replace('#', '')
+    line = line.replace(',', '')
+    line = line.replace('.', '')
+    line = line.replace(':', '')
+    line = line.replace('--', '')
+    line = line.replace('"', '')
+    line = line.replace('?', '')
+    line = line.replace('(', '')
+    line = line.replace(')', '')
+
+    # line=re.split(r'\W+',line)
+    wo = line.split(' ')
+    for word in wo:
+        # print (word)
+        word = lemmatizer.lemmatize(word.lower())
+        if word not in stop_words and word != '':
+            BOW[word] = BOW.get(word, 0) + 1
+
+string_list = []
+string_list_new = []
+import json
+import json
+
+for word in BOW:
+    word = word.encode('ascii')
+    string = "{text: '" + word + "', size: " + str(BOW[word]) + "}"
+    # print string
+    string_list.append(string)
