e2a3b2d8506d36765d33effb0e898d3658a07cd5
yiwen zeng <cocozyw2811@gmail.com>
2018-05-14 17:38:32 +1000

    add social icon to force directed graph

--- mysite/static/js/forceGraph.js
+++ mysite/static/js/forceGraph.js
@@ -3,38 +3,115 @@ function drawForceGraph(data, htmlElem){
 		width = +svg.attr("width"),
 		height = +svg.attr("height");
 
-	var color = d3.scaleOrdinal(d3.schemeCategory20);
+	// var color = d3.scaleOrdinal(d3.schemeCategory20);
+	var color_palettes = ['#4abdac', '#fc4a1a', '#f7b733', '#f03b20', '#feb24c', '#ffeda0', '#007849', '#0375b4', '#ffce00', '#373737', '#dcd0c0', '#c0b283', '#e37222', '#07889b', '#eeaa7b', '#062f4f', '#813772', '#b82601', '#565656', '#76323f', '#c09f80'];
+	
+	var color = d3.scaleOrdinal(color_palettes);
 
 	var simulation = d3.forceSimulation()
 		.force("link", d3.forceLink().id(function(d) { return d.id; }))
 		.force("charge", d3.forceManyBody().strength(-1500))
 		.force("center", d3.forceCenter(width / 2, height / 2));
+		
+	var defs = svg.append('svg:defs');
+	
+	var config = {
+		"avatar_size" : 60
+	}
 
-	  var link = svg.append("g")
-		  .attr("class", "links")
+	defs.append("svg:pattern")
+		.attr("id", "twitter")
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("patternUnits", "patternContentUnits")
+		.append("svg:image")
+		.attr("xlink:href", '../static/img/tw.png')
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("x", 0)
+		.attr("y", 0);
+	
+	defs.append("svg:pattern")
+		.attr("id", "facebook")
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("patternUnits", "patternContentUnits")
+		.append("svg:image")
+		.attr("xlink:href", '../static/img/fb.png')
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("x", 0)
+		.attr("y", 0);
+		
+	defs.append("svg:pattern")
+		.attr("id", "tumblr")
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("patternUnits", "patternContentUnits")
+		.append("svg:image")
+		.attr("xlink:href", '../static/img/tu.png')
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("x", 0)
+		.attr("y", 0);
+	
+	defs.append("svg:pattern")
+		.attr("id", "reddit")
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("patternUnits", "patternContentUnits")
+		.append("svg:image")
+		.attr("xlink:href", '../static/img/re.png')
+		.attr("width", config.avatar_size)
+		.attr("height", config.avatar_size)
+		.attr("x", 0)
+		.attr("y", 0);
+		
+	var link = svg.append("g")
+		.attr("class", "links")
 		.selectAll("line")
 		.data(data.links)
 		.enter().append("line")
 		  .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-	  var node = svg.append("g")
-		  .attr("class", "nodes")
+	var node = svg.append("g")
+		.attr("class", "nodes")
 		.selectAll("circle")
 		.data(data.nodes)
 		.enter().append("circle")
-		  .attr("r", 15)
-		  .attr("fill", function(d) { return color(d.group); })
-		  .call(d3.drag()
-			  .on("start", dragstarted)
-			  .on("drag", dragged)
-			  .on("end", dragended));
-	  var text_dx = -20;
-	  var text_dy = 20;
+		.attr("r", function(d){
+			if (d.id == 'twitter' || d.id == 'facebook' || d.id == 'tumblr' || d.id == 'reddit'){
+				return 30;
+			} else {
+				return 15;
+			}
+		})
+		.attr("fill", function(d) {
+				if (d.id == 'twitter'){
+					return "url(#twitter)";
+				} else if (d.id == 'facebook'){
+					return "url(#facebook)";
+				} else if (d.id == 'tumblr'){
+					return "url(#tumblr)";
+				} else if (d.id == 'reddit'){
+					return "url(#reddit)";
+				} else {
+					return color(d.group); 
+				}
+			})
+		.call(d3.drag()
+				.on("start", dragstarted)
+				.on("drag", dragged)
+				.on("end", dragended));
+	  var text_dx = -25;
+	  var text_dy = 40;
 	  var nodes_text = svg.selectAll(".nodetext")
 									.data(data.nodes)
 									.enter()
 									.append("text")
 									.attr("class","nodetext")
+									.style("font-family", "azo-sans-web")
+									.style("stroke", "white")
 									.attr("dx",text_dx)
 									.attr("dy",text_dy)
 									.text(function(d){
