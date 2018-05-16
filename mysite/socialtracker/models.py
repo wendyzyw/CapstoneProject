be12706b86a0f890e7967deacbd02eb95a7b7b87
RegexKenny <31911769+RegexKenny@users.noreply.github.com>
2018-05-16 11:50:16 +1000

    user_profile update

--- mysite/socialtracker/models.py
+++ mysite/socialtracker/models.py
@@ -29,6 +29,15 @@ from django.contrib.auth.models import AbstractUser
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    phone = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    zip_code = models.CharField(max_length=100, blank=True)
+    gender = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.username
