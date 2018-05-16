87e7b12b58291f55e9d0c494c042a20b94ccba55
WShowshow <honeybearear@gmail.com>
2018-05-16 10:01:09 +1000

    Revert "fix some bug"
    This reverts commit 744585466bdb2f4e37ea7d5fdad95d682b9b96c1.

--- mysite/mysite/static/js/control.js
+++
@@ -1,5 +0,0 @@
-$("#keywords").click(function(e) {
-        e.preventDefault();
-        $('.data-leftnav').removeClass('.data-leftnav-active');
-        $(this).addClass('.data-leftnav-active');
-    });
